fb011462c4fbed1d8afa28edb031145c3ba6cbbb
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-29 04:56:29 -0400

    Landing Page

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,9 +1,34 @@
 import React, {Component} from 'react';
+import {Cell, Grid} from "react-mdl";
 
 class LandingPage extends Component {
     render() {
         return (
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                        <img
+                            src={process.env.PUBLIC_URL + '/rahul.png'}
+                            className='avatar-img'
+                            alt="avatar"
+                        />
+                        <div className="banner-text">
+                            <h1>Software Developer</h1>
+                            <hr />
+                            <p>HTML/CSS | React | Angular | Java | Oracle</p>
+                            <div className="social-links">
+                                <a href="https://github.com/rahul-wahi" target="_blank">
+                                    <i className="fa fa-linkedin-square" aria-hidden="true"/>
+                                </a>
+                                <a href="https://github.com/rahul-wahi" target="_blank">
+                                    <i className="fa fa-github-square" aria-hidden="true"/>
+                                </a>
+                            </div>
+                        </div>
+
+                    </Cell>
+                </Grid>
+            </div>
         )
     };
 }
