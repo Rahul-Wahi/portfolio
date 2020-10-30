1c9eaef6aa026ce54e546191c905dfb950e4b4bf
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-29 21:46:48 -0400

    Resume Data

---
+++ src/resumeData.js
@@ -0,0 +1,141 @@
+let resumeData = {
+    "imagebaseurl": "https://ivy555.github.io/",
+    "name": "Rahul Wahi",
+    "role": "Software Engineer",
+    "linkedinId": "rahul-wahi",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to learn and create applications in various parts of software development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks": [
+        {
+            "name": "linkedin",
+            "url": "https://www.linkedin.com/in/rahul-wahi/",
+            "className": "fa fa-linkedin"
+        },
+        {
+            "name": "github",
+            "url": "http://github.com/rahul-wahi",
+            "className": "fa fa-github"
+        }
+    ],
+    "aboutme": "Currently a computer science graduate student at the University of Florida. I have 3+ years of experience as a Software Engineer working in the Agile Model. I am a highly motivated & result oriented Software Engineer with excellent problem-solving skills & a keen interest in developing Large Scale Distributed Systems and Web-applications.",
+    "keySkills": ["Experience working in large scale backend application using AWS SWF, Spring, Java",
+    "Expertise in Web application development using Html/CSS, Angular JS, Typescript, Java Spring Boot, Oracle DB",
+    "Exposure to distributed systems by implementing Tapestry p2p overlay network, Twitter Clone, Bittorrent Clone, Network File System.",
+    "Experience working with Docker, Containers, Jenkins, Jira."],
+    "address": "USA",
+    "website": "https://ivy555.github.io",
+    "education": [
+        {
+            "UniversityName": "University of Florida",
+            "specialization": "MS- CS",
+            "endMonth": "May",
+            "endYear": "2021",
+            "Achievements": "Some Achievements",
+            "GPA": "3.88 / 4"
+        },
+        {
+            "universityName": "Delhi Technological University",
+            "specialization": "BTech- ECE",
+            "endMonth": "May",
+            "endYear": "2015",
+            "startMonth": "Aug",
+            "startYear": "2011",
+            "Achievements": "Some Achievements",
+            "CGPA": "8.25 / 10"
+        }
+    ],
+    "work": [
+        {
+            "companyName": "Amazon Web Services",
+            "country": "USA",
+            "role": "SDE Intern",
+            "endMonth": "Aug",
+            "endYear": "2020",
+            "startMonth": "Jun",
+            "startYear": "2020",
+            "technologies": "Java, Spring Boot, SQL, Amazon SWF, Amazon EC2",
+            "description": ["•\tWorked in an Agile model to design and implement the metric collection for upgrade process in RDS (managing MySQL, Postgres, MariaDB) to automate the collection of various DB upgrade statistics for 1000 of DB instances."
+                , "•\tThese metrics will save the manual efforts of scanning the large log files and querying the DB for tracking the upgrade progress and detecting bottlenecks in the process."
+                , "•\tRe-architected the existing system for improving the maintainability of the upgrade process."]
+        },
+        {
+            "companyName": "Aricent",
+            "country": "India",
+            "role": "Software Engineer",
+            "endMonth": "July",
+            "endYear": "2019",
+            "startMonth": "Nov",
+            "startYear": "2015",
+            "technologies": "Java, Spring Boot, SQL, Amazon SWF, Amazon EC2",
+            "description": ["•\tParticipated in agile ceremonies (PI planning, Sprint planning, Sprint Retrospective) to break down the epics to story, story point estimation, grooming user stories.",
+            "•\tTook initiatives to implement Test Case Prioritization tool in Python, and Log utilities in Perl that led to a 30% increase in team productivity",
+            "•\tDesigned an innovative Combinatorial Automation framework from scratch to test REST APIs. This framework replaced the third-party framework which leads to an increase in team size from 3 to 6",
+            "•\tContributed to the keyword-driven framework in QTP for automation testing of flex-based UI application and to Java/Selenium based Automation framework for automated testing of Web application and delivering 30 Automated test per month"]
+        }
+    ],
+    "skillsDescription": "Your skills here",
+    "skills": [
+        {
+            "skillname": "HTML5"
+        },
+        {
+            "skillname": "CSS"
+        },
+        {
+            "skillname": "Reactjs"
+        }
+    ],
+    "projects": [
+        {
+            "name": "Network File System",
+            "tags":["Distributed Systems", "File System"],
+            "technologies": "Python, Networking, Git",
+            "description": ["•\tCreating RPC-based NFS to run on a multi client-server system with logging, virtualization, and data redundancy"],
+            "img": "images/portfolio/phone.jpg",
+            "github":""
+        },
+        {
+            "name": "BitTorrent Clone",
+            "tags":["Distributed Systems", "Socket Programming"],
+            "technologies": "Java, Socket Programming, Multithreading, Git",
+            "description": ["•\tImplementing Peer-to-Peer File Transfer using the BitTorrent file transfer protocol"],
+            "img": "images/portfolio/project.jpg",
+            "github":""
+        },
+        {
+            "name": "Twitter Clone Simulation",
+            "tags":["Web", "Distributed Systems"],
+            "technologies": "Elixir, HTML, CSS, JavaScript, Phoenix Framework, ExUnit, ETS Storage",
+            "description": ["•\tCloned the twitter functionalities using the elixir phoenix framework.",
+            "•\tEvaluated the twitting and querying efficiency by running a network of 1000 actors simulating the zipf-distribution behavior"],
+            "img": "images/portfolio/project2.png",
+            "github":""
+        },
+        {
+            "name": "Tapestry Overlay",
+            "tags":[],
+            "technologies": "Java, Spring Boot, SQL, Amazon SWF, Amazon EC2",
+            "description": ["•\tImplemented a paper on ‘Resilient Tapestry Overlay Network’ using DHTs and back pointers. Evaluated the robustness of this P2P overlay network with 1000 nodes and 100 requests per node."],
+            "img": "images/portfolio/phone.jpg",
+            "github":""
+        },
+        {
+            "name": "Web Application for Visual Data Analytics",
+            "tags":["Web", "Angular"],
+            "technologies": "HTML, CSS, JavaScript, AngularJS, Java, Spring Boot, OracleDB, Database Design",
+            "description": ["•\tLead a team of 3 and developed an end-to-end Web application to provide visual analysis of 150,000 crime data showing different trends. "],
+            "img": "images/portfolio/phone.jpg",
+            "github":""
+        },
+        {
+            "name": "Task Scheduler",
+            "tags":["Advance Data Structures"],
+            "technologies": "Java, Git",
+            "description": ["Implemented min heap and red-black tree for creating the task scheduler application"],
+            "img": "images/portfolio/phone.jpg",
+            "github":""
+        }
+    ]
+}
+
+export default resumeData
