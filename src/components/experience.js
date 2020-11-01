87b8701f4c8ad7ae06678dd0afa87ef11d8b92b5
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-31 22:00:32 -0400

    Render experience description array as li tags

--- src/components/experience.js
+++ src/components/experience.js
@@ -4,17 +4,22 @@ import { Grid, Cell } from 'react-mdl';
 class Experience extends Component {
     render() {
         const experience = this.props.experience;
+        const description = experience.description.map(workDescription =>
+            <li style={{marginTop:'0px', marginLeft:'1em', fontSize:'15px', textAlign:'justify'}}><span>{workDescription}</span></li>
+        );
         return(
             <Grid>
                 <Cell col={8}>
                     <h4 style={{marginTop:'0px'}}>{experience.role}, {experience.companyName}, {experience.country}</h4>
                     <h6 style={{marginTop:'0px', marginLeft:'1em'}}><span>Technologies:</span> {experience.technologies}</h6>
-                    <p style={{marginTop:'0px', marginLeft:'2em'}}>{experience.description}</p>
                 </Cell>
 
+
                 <Cell col={4}>
-                    <p>{experience.startMonth} {experience.startYear} - {experience.endMonth} {experience.endYear}</p>
+                    <p className="duration">{experience.startMonth} {experience.startYear} - {experience.endMonth} {experience.endYear}</p>
                 </Cell>
+
+                {description}
 
             </Grid>
         )
