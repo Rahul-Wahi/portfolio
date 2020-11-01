b9be241b858150af8d31b83f68c5b80e6c1c0215
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-31 22:02:55 -0400

    make header fixed to screen size and get name form resume data

--- src/App.js
+++ src/App.js
@@ -2,30 +2,32 @@ import './App.css';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main'
 import {Link} from "react-router-dom";
+import resumeData from "./resumeData";
+
 function App() {
   return (
 
-      <div className="demo-big-content">
-        <Layout>
-            <Header className="header-color" title="Title" scroll>
+      <div   className="demo-big-content" >
+        <Layout fixedHeader>
+            <Header className="header-color" title={<Link style={{textDecoration:'none', color:'white'}} to="/">{resumeData.name}</Link>} scroll>
                 <Navigation>
+                    <Link to="/">Home</Link>
                     <Link to="/resume">Resume</Link>
-                    <Link to="/aboutme">About Me</Link>
                     <Link to="/projects">Projects</Link>
                     <Link to="/contact">Contact</Link>
                 </Navigation>
             </Header>
-            <Drawer title="Title">
+            <Drawer  title={<Link style={{textDecoration:'none', color:'grey'}} to="/">{resumeData.name}</Link>}>
                 <Navigation>
                     <Link to="/resume">Resume</Link>
-                    <Link to="/aboutme">About Me</Link>
                     <Link to="/projects">Projects</Link>
                     <Link to="/contact">Contact</Link>
                 </Navigation>
             </Drawer>
+
             <Content>
                 <div className="page-content" />
-                <Main />
+                <Main resumeData={resumeData}/>
             </Content>
         </Layout>
     </div>
