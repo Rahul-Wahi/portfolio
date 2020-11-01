998eb2411a39d38c55ba906b52ee77ce428ec95a
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-31 21:56:10 -0400

    Redirect to github project page on button click

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,59 +4,60 @@ import {Card, CardText, CardActions, CardTitle, Cell, Grid, Tab, Tabs, Button, C
 class Project extends Component {
     constructor(props) {
         super(props);
-        this.state = {activeTab:0};
+        this.state = {activeTab: 0};
     }
 
-    changeCategory () {
+    handleClick(url) {
+        window.open(url);
+    }
+
+    changeCategory() {
+
         if (this.state.activeTab === 0) {
+            const projects = this.props.resumeData.projects.map(project =>
+                <div className="project">
+                    <Card shadow={5} style={{minWidth: '450px', margin: 'auto'}}>
+                        <h3>{project.name}</h3>
+                        <CardTitle style={{
+                            color: 'black', height: '200px', background:
+                                'url(' + project.img + ') center / cover'
+                        }}>
+                        </CardTitle>
+                        <CardText><span>Technologies: </span>{project.technologies}
+                            {project.description.map(descriptions =>
+                                <li style={{
+                                    marginTop: '0px',
+                                    marginLeft: '0',
+                                    fontSize: '15px',
+                                    textAlign: 'justify'
+                                }}>{descriptions}</li>
+                            )}</CardText>
+                        <CardActions border>
+                            {project.github.length > 0 &&
+                            <Button onClick={() => this.handleClick(project.github[0])} colored>Github</Button>
+
+                            }
+
+                        </CardActions>
+                        <CardMenu style={{color: '#ffff'}}>
+                            <Icon name='share'/>
+                        </CardMenu>
+                    </Card>
+                </div>
+            );
             return (
                 <div className="projects-grid">
-                <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                    <CardTitle style={{color:'black', height: '176px', background:
-                    'url(https://www.flaticon.com/svg/static/icons/svg/124/124021.svg) center / cover'}}>
-                     Twitter Clone
-                    </CardTitle>
-                    <CardText>
-                        sdjkbasdhv c sckjabdcjkxn cc
-                        xbjc jx ckh nbz
-                        KB jnxm ,jxk. ,nmn,kjz,bbzn,b
-                    </CardText>
-
-                    <CardActions border>
-                        <Button colored>Github</Button>
-                    </CardActions>
-                    <CardMenu style={{color: '#ffff'}}>
-                        <Icon name='share'/>
-                    </CardMenu>
-                </Card>
-            <Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-                <CardTitle style={{color:'black', height: '176px', background:
-                        'url(https://www.flaticon.com/svg/static/icons/svg/124/124021.svg) center / cover'}}>
-                    Twitter Clone
-                </CardTitle>
-                <CardText>
-                    sdjkbasdhv c sckjabdcjkxn cc
-                    xbjc jx ckh nbz
-                    KB jnxm ,jxk. ,nmn,kjz,bbzn,b
-                </CardText>
-
-                <CardActions border>
-                    <Button colored>Github</Button>
-                </CardActions>
-                <CardMenu style={{color: '#ffff'}}>
-                    <Icon name='share'/>
-                </CardMenu>
-            </Card>
+                    {projects}
                 </div>
-                )
+            )
         }
     }
+
     render() {
         return (
             <div className="category-tabs">
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab : tabId})} ripple>
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({activeTab: tabId})} ripple>
                     <Tab>Projects</Tab>
-                    <Tab>Projects1</Tab>
                 </Tabs>
                 <section>
                     <Grid>
