1fa3f89970799d20a1a53d338d41d2eda5dbaed5
Rahul Wahi <rahulwahi42@gmail.com>
2020-10-31 21:58:11 -0400

    Implement Contact Page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,9 +1,45 @@
 import React, {Component} from 'react';
+import {Cell, Grid, List, ListItem, ListItemContent} from "react-mdl";
+
 
 class Contact extends Component {
     render() {
         return (
-            <div><h1>Contact Page</h1></div>
+            <div className="contact-body">
+                <Grid className="contact-grid">
+                    <Cell col={6} >
+                        <h2>{this.props.resumeData.name}</h2>
+                        <img src={this.props.resumeData.profileImage}
+                        alt="avatar"
+                        style={{height: '250px'}}/>
+                        <p style={{width:'75%', margin:'auto', paddingTop:'1em', textAlign:'justify'}}>
+                            {this.props.resumeData.aboutme}
+                        </p>
+                    </Cell>
+                    <Cell col={6}>
+
+                        <h2>Contact me</h2>
+                        <hr/>
+                        <div className="contact-list">
+                            <List>
+                                <ListItem>
+                                    <ListItemContent style={{fontFamily: 'Anton', fontSize:'25px'}}>
+                                        <i className="fa fa-phone" aria-hidden="true"/>
+                                        {this.props.resumeData.phone}
+                                    </ListItemContent>
+                                </ListItem>
+                                <ListItem>
+                                    <ListItemContent style={{fontFamily: 'Anton', fontSize:'25px'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        {this.props.resumeData.mail}
+                                    </ListItemContent>
+                                </ListItem>
+                            </List>
+                        </div>
+
+                    </Cell>
+                </Grid>
+            </div>
         )
     };
 }
